80841f0ffbc6eaf132ab7fb114e78196db88dd77
Brian Lee <briandl92391@gmail.com>
2017-02-09 11:35:31 -0800

    Updated setup.py to show new name

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(
-    name='sftpdownloader',
+    name='tractor',
     version='0.1.0',
     description='Module used to download files over SSH',
-    url='https://github.com/orionnetworksolutions/sftpdownloader',
+    url='https://github.com/orionnetworksolutions/tractor',
     author='Brian Lee',
     author_email='briandl92391@gmail.com',
     license='MIT',
-    packages=['sftpdownloader'],
+    packages=['tractor'],
     install_requires=['paramiko'],
     zip_safe=True
 )
