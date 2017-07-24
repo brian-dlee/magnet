9566797dbc7b1a5e913a309e8ac6530f1e298bee
Brian Lee <briandl92391@gmail.com>
2017-07-24 10:11:26 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
     name='tractor',
-    version='0.1.0',
+    version='0.1.1',
     description='Module used to download files over SSH',
     url='https://github.com/orionnetworksolutions/tractor',
     author='Brian Lee',
     author_email='briandl92391@gmail.com',
     license='MIT',
     packages=['tractor'],
-    install_requires=['paramiko'],
+    install_requires=['paramiko', 'bunch'],
     zip_safe=True
 )
